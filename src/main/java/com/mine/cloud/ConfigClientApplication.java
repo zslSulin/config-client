dbac5b0ce93bd5d27bae08c418e9ae16850ba719
张苏林 <zhangsl@51jnq.com>
2018-11-12 11:30:11 +0800

    配置中心 客户端初始

--- src/main/java/com/mine/cloud/ConfigClientApplication.java
+++ src/main/java/com/mine/cloud/ConfigClientApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
 }
